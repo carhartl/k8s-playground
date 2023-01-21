773e573c57b633c59829ff019d08b7f3c50adf85
Klaus Hartl <klaus.hartl@gmail.com>
2023-01-21 14:18:25 +0100

    Refactor repo tests to use suite from testify

--- app/internal/repositories/peoplerepo/yugabyte_repository_test.go
+++ app/internal/repositories/peoplerepo/yugabyte_repository_test.go
@@ -1,59 +1,74 @@
 package peoplerepo
 
 import (
+	"database/sql"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/carhartl/playground/internal/core/domain"
+	"github.com/carhartl/playground/internal/core/ports"
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestSave(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
+type Suite struct {
+	suite.Suite
+	mock sqlmock.Sqlmock
+	repo ports.PeopleRepository
+}
 
-	mock.ExpectBegin()
-	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "people" ("email","first_name","last_name","id") 
+func (s *Suite) SetupSuite() {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, s.mock, err = sqlmock.New()
+	require.NoError(s.T(), err)
+
+	orm, err := gorm.Open("postgres", db)
+	require.NoError(s.T(), err)
+	orm.LogMode(true)
+
+	s.repo = New(orm)
+}
+
+func (s *Suite) TestSave() {
+	s.mock.ExpectBegin()
+	s.mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "people" ("email","first_name","last_name","id") 
                                         VALUES ($1,$2,$3,$4) RETURNING "people"."id"`)).
 		WithArgs("foo@example.com", "foo", "bar", "be2ae927-ad56-4e85-b75c-9101185c3b6b").
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow("be2ae927-ad56-4e85-b75c-9101185c3b6b"))
-	mock.ExpectCommit()
+	s.mock.ExpectCommit()
 
-	orm, err := gorm.Open("postgres", db)
-	require.NoError(t, err)
-	orm.LogMode(true)
-	repo := New(orm)
-	err = repo.Save(domain.Person{
+	err := s.repo.Save(domain.Person{
 		Email:     "foo@example.com",
 		FirstName: "foo",
 		LastName:  "bar",
 		Uuid:      uuid.MustParse("be2ae927-ad56-4e85-b75c-9101185c3b6b"),
 	})
-	require.NoError(t, err)
+	require.NoError(s.T(), err)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
+	if err := s.mock.ExpectationsWereMet(); err != nil {
+		s.T().Errorf("There were unfulfilled expectations: %s", err)
 	}
 }
 
-func TestFindByID(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "people" WHERE (id::text = $1) ORDER BY "people"."id" ASC LIMIT 1`)).
+func (s *Suite) TestFindByID() {
+	s.mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "people" WHERE (id::text = $1) ORDER BY "people"."id" ASC LIMIT 1`)).
 		WithArgs("foo")
 
-	orm, err := gorm.Open("postgres", db)
-	require.NoError(t, err)
-	orm.LogMode(true)
-	repo := New(orm)
-	_, err = repo.FindByID("foo")
-	require.NoError(t, err)
+	_, err := s.repo.FindByID("foo")
+	require.NoError(s.T(), err)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
+	if err := s.mock.ExpectationsWereMet(); err != nil {
+		s.T().Errorf("There were unfulfilled expectations: %s", err)
 	}
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(Suite))
 }
